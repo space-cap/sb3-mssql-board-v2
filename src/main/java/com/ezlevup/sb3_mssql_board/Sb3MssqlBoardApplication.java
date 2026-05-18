f5b2fdd3fa6355563121a05ab3d2a11e95403bd6
이영현 <hong.git@yahoo.com>
2026-05-18 16:46:35 +0900

    시작

---
+++ src/main/java/com/ezlevup/sb3_mssql_board/Sb3MssqlBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ezlevup.sb3_mssql_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3MssqlBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3MssqlBoardApplication.class, args);
+	}
+
+}
